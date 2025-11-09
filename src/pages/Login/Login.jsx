5135171bdc19048325b70a7c9521ce622b3a4512
Yanz06 <kurtyanzieeusebio@gmail.com>
2025-11-09 18:10:43 +0800

    Modular CSS and Login Page Structure
    - Modular CSS structure added
    - Login page structure built
    - Branch by Yanzie

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,6 +4,8 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase-config';
 import { useState } from 'react';
 
+import logo from "../../assets/images/react.svg";
+
 // Test: Login folder still exists
 
 function Login() {
@@ -25,12 +27,30 @@ function Login() {
   return (
     <div className='login-container'>
 
-      <div className='login-card'>
+      <div className='login-left-section'>
+        <img src={logo} alt="" />
+      </div>
 
-        <h1>Login Page</h1>
-        <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder='Email'/>
-        <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Password'/>
-        <button onClick={handleLogin}>Login</button>
+      <div className='login-right-section'>
+
+        <div className='header'>
+          <h1 className='title-bold'>Login Page</h1>
+          <p className='subtitle-regular'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi commodi exercitationem, fuga quisquam molestias dolorem hic cumque ratione eveniet, omnis distinctio repellat nesciu</p>
+        </div>
+
+        <div className='login-forms'>
+          <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder='Email'/>
+          <input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Password'/>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+        <p>or</p>
+
+        <div className='quick-login'>
+          <button>Continue with Google</button>
+          <button>Continue with Apple</button>
+        </div>
+        
+        
         <p>Don't have an Account? <NavLink to="/register">Register</NavLink></p>
 
       </div>
