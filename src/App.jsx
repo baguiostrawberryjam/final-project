32325aff50c641e4585f0a5523bada5a8aebe3b3
Jazrel <jazrelsimonevangelista@gmail.com>
2025-11-09 13:49:36 +0800

    Fixed Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "./firebase-config";
