3a761bb797aeb451d9432a97724ac0b69d81a741
baguiostrawberryjam <johnuelleangel@gmail.com>
2025-11-10 17:53:08 +0800

    Added Firebase Storage integration (#11)

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import './profile.css'
+import UserProfile from "../components/UserProfile/UserProfile";
+
+function Profile() {
+    return(
+        <>
+            <UserProfile />
+        </>
+    )
+}
+
+export default Profile;
