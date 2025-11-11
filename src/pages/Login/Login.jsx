df5cefa16f35d5a4eb536a5222f1843ba2d83fe0
baguiostrawberryjam <johnuelleangel@gmail.com>
2025-11-11 13:10:34 +0800

    Fixes for Login Page Frontend (#15)
    * Fixes for Login Page
    
    * Minor fixes to Login Page Frontend fix

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -34,10 +34,10 @@ function Login() {
     setEmail(email)
 
     if(email.trim().length <= 0){
-        setEmailError("Input Required");
+        setEmailError("Input required");
         setIsEmailValid(false);
     } else if(email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)==null){
-        setEmailError("Invalid Email");
+        setEmailError("Invalid email");
         setIsEmailValid(false);
     } else {
         setEmailError("");
@@ -55,10 +55,10 @@ function Login() {
       const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\S+$).{8,20}$/
 
       if(pass.trim().length <= 0){
-          setPasswordError("Input Required");
+          setPasswordError("Input required");
           setIsPasswordValid(false);
       } else if(!passwordRegex.test(pass)){
-          setPasswordError("Invalid Password");
+          setPasswordError("Invalid password");
           setIsPasswordValid(false);
       } else {
           setPasswordError("");
@@ -106,7 +106,7 @@ function Login() {
         </div>
         
         
-        <p>Don't have an Account? <NavLink to="/register">Register</NavLink></p>
+        <p>Don't have an account? <NavLink to="/register" className="link-text">Register</NavLink></p>
 
       </div>
       
